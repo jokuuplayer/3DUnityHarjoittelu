f2ed13f942e0435e792895146b242eeec7b7f611
Your Name <you@example.com>
2023-11-28 18:09:24 +0200

    kameran kääntely

--- TeodorHelminenUnity3D/Assets/PlayerController.cs
+++ TeodorHelminenUnity3D/Assets/PlayerController.cs
@@ -7,11 +7,20 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5.5f;
     public CharacterController controller;
 
+    public float mouseSpeed = 3f;
+
+    public CursorLockMode cursorLock;
+
+    private float mouseMovementX = 0f;
+    private float mouseMovementY = 0f;
+
+
     Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = cursorLock;
+        Cursor.visible =(CursorLockMode.Locked != cursorLock);
     }
 
     // Update is called once per frame
@@ -20,7 +29,15 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        mouseMovementX += Input.GetAxis("Mouse X") * mouseSpeed;
+        mouseMovementY -= Input.GetAxis("Mouse Y") * mouseSpeed;
+        
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseMovementY,0,0);
+        transform.localRotation = Quaternion.Euler(0,mouseMovementX,0);
+
+
         moveDir = new Vector3(horizontal,0,vertical);
+        moveDir = transform.rotation * moveDir;
 
         controller.SimpleMove(moveDir * moveSpeed);
 
